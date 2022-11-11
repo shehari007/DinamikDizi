7b9ed8783d3b4a3a5dd301f5e9aac16352596566
Muhammad Sheharyar Butt <56956294+shehari007@users.noreply.github.com>
2022-11-11 13:17:17 +0300

    sil veya ekle fonksyonu guncellendi

--- src/dinamikdiziodev/DinamikDizi.java
+++ src/dinamikdiziodev/DinamikDizi.java
@@ -1,108 +1,92 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dinamikdiziodev;
 
-
-
-/**
- *
- * @author sheharyar
- */
 import java.util.Arrays;
 
 public class DinamikDizi {
-    private int dizi[];    
+
+    private int dizi[];
     private int boyut;
     private int kapasite;
-    
-    public DinamikDizi(){
+
+    public DinamikDizi() {
         dizi = new int[5];
-        boyut=0;
-        kapasite=5;
+        boyut = 0;
+        kapasite = 4;
     }
-     
-    public void addElement(int element){
-        if (boyut == kapasite){ 
+
+    public void addElement(int element) {
+        if (boyut == kapasite) {
+            System.out.println("Dizide Bos yer yok.. ");
+        } else {
+            dizi[boyut] = element;
+            boyut++;
         }
-        dizi[boyut] = element;
-        boyut++;
     }
-            public void addElement(int index, int element){
-        if (boyut == kapasite){
+
+    public void addElement(int index, int element) {
+        if (boyut == kapasite) {
+            System.out.println("Dizide Bos yer yok.. ");
+        } else {
+            dizi[index] = element;
+            boyut++;
         }
-        dizi[index] = element;
-        boyut++;
     }
-            // to remove an element at a particular index
-    public void sil(int index){
-        if(index>=boyut || index<0){
-            System.out.println("Bu indexe Bir Eleman yok");
-        }else{
-            for(int i=index;i<boyut-1;i++){
-                dizi[i] = dizi[i+1];
-            }
-            dizi[boyut-1]=0;
+
+    public void sil(int index) {
+        if (index >= boyut || index < 0) {
+            System.out.println("Bu indexte Eleman yok");
+        } else {
+            dizi[index] = 0;
             boyut--;
-        }}
-        
-        public int Boy(){
+        }
+    }
+
+    public int Boy() {
         return boyut;
     }
-     
-    
-    public int Kapa(){
+
+    public int Kapa() {
         return kapasite;
     }
-     
-    
-    public void printElements(){
-        System.out.println("Dizinin elemanlar :"+Arrays.toString(dizi));
-          }
-     public static void main(String args[]){
-       DinamikDizi dizi = new DinamikDizi();
-       dizi.printElements();
-       
-      //boş dizinin veri ekle metodu ile veri eklendi  
+
+    public void printElements() {
+        System.out.println("Dizinin elemanlar :" + Arrays.toString(dizi));
+    }
+
+    public static void main(String args[]) {
+        DinamikDizi dizi = new DinamikDizi();
+        dizi.printElements();
+
+        //boş dizinin veri ekle metodu ile veri eklendi  
         dizi.addElement(1);
         dizi.addElement(2);
-        
-        System.out.println("Dizi Boyut :"+dizi.Boy()+
-            " Ve Dizi Kapasite :"+dizi.Kapa());
+
+        System.out.println("Dizi Boyut :" + dizi.Boy()
+                + " Ve Dizi Kapasite :" + dizi.Kapa());
         dizi.printElements();
-        
+
         dizi.addElement(3);
-        System.out.println("Dizi Boyut :"+dizi.Boy()+
-            " Ve Kapasite :"+dizi.Kapa());
+        System.out.println("Dizi Boyut :" + dizi.Boy()
+                + " Ve Kapasite :" + dizi.Kapa());
         dizi.printElements();
-        
+
         //Dizinin index ile eleman eklendi
-        dizi.addElement(1,5);
-        System.out.println("Dizi Boyut :"+dizi.Boy()+
-            " Ve Kapasite :"+dizi.Kapa());        
+        dizi.addElement(1, 5);
+        System.out.println("Dizi Boyut :" + dizi.Boy()
+                + " Ve Kapasite :" + dizi.Kapa());
         dizi.printElements();
-        
+
         //Dizinin 2. index'de Eleman Sildi
         dizi.sil(2);
-        System.out.println("Dizi Boyut :"+dizi.Boy()+
-            " Ve Kapasite :"+dizi.Kapa());        
+        System.out.println("Dizi Boyut :" + dizi.Boy()
+                + " Ve Kapasite :" + dizi.Kapa());
         dizi.printElements();
-        
+
         //5. index boş
-         System.out.println("Dizinin 5. index'de Eleman Silecek");
+        System.out.println("Dizinin 5. index'de Eleman Silecek");
         dizi.sil(5);
-         dizi.printElements();
-         System.out.println("Dizi Boyut :"+dizi.Boy()+
-            " Ve Kapasite :"+dizi.Kapa());
-     }
+        dizi.printElements();
+        System.out.println("Dizi Boyut :" + dizi.Boy()
+                + " Ve Kapasite :" + dizi.Kapa());
+    }
 }
-        
-    
-  
-        
-    
-    
-
-
